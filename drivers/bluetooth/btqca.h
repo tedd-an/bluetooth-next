880cdfce7addb424bf4f15d50fa732f810f0590b
Venkata Lakshmi Narayana Gubba <gubbaven@codeaurora.org>
2020-11-18 18:22:23 +0530

    Bluetooth: btqca: Add support to read FW build version for WCN3991 BTSoC
    Add support to read FW build version for WCN3991 BTSoC
    
    Signed-off-by: Venkata Lakshmi Narayana Gubba <gubbaven@codeaurora.org>

--- drivers/bluetooth/btqca.h
+++ drivers/bluetooth/btqca.h
@@ -11,6 +11,7 @@
 #define EDL_PATCH_CMD_LEN		(1)
 #define EDL_PATCH_VER_REQ_CMD		(0x19)
 #define EDL_PATCH_TLV_REQ_CMD		(0x1E)
+#define EDL_GET_BUILD_INFO_CMD		(0x20)
 #define EDL_NVM_ACCESS_SET_REQ_CMD	(0x01)
 #define MAX_SIZE_PER_TLV_SEGMENT	(243)
 #define QCA_PRE_SHUTDOWN_CMD		(0xFC08)
@@ -33,6 +34,8 @@
 
 #define QCA_HCI_CC_OPCODE		0xFC00
 #define QCA_HCI_CC_SUCCESS		0x00
+
+#define QCA_FW_BUILD_VER_LEN		255
 
 enum qca_baudrate {
 	QCA_BAUDRATE_115200 	= 0,
