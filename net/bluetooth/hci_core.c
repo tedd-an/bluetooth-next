399a24e972ec6e24a62c9656d8fd40c6f96dc6f5
Kiran K <kiran.k@intel.com>
2021-05-02 14:52:07 +0530

    Bluetooth: Add support for Read Local Supported Codecs V2
    Use V2 version of read local supported command is controller
    supports
    
    snoop:
    > HCI Event: Command Complete (0x0e) plen 20
          Read Local Supported Codecs V2 (0x04|0x000d) ncmd 1
            Status: Success (0x00)
            Number of supported codecs: 7
              Codec: u-law log (0x00)
              Logical Transport Type: 0x02
                Codec supported over BR/EDR SCO and eSCO
              Codec: A-law log (0x01)
              Logical Transport Type: 0x02
                Codec supported over BR/EDR SCO and eSCO
              Codec: CVSD (0x02)
              Logical Transport Type: 0x02
                Codec supported over BR/EDR SCO and eSCO
              Codec: Transparent (0x03)
              Logical Transport Type: 0x02
                Codec supported over BR/EDR SCO and eSCO
              Codec: Linear PCM (0x04)
              Logical Transport Type: 0x02
                Codec supported over BR/EDR SCO and eSCO
              Codec: Reserved (0x08)
              Logical Transport Type: 0x03
                Codec supported over BR/EDR ACL
                Codec supported over BR/EDR SCO and eSCO
              Codec: mSBC (0x05)
              Logical Transport Type: 0x03
                Codec supported over BR/EDR ACL
                Codec supported over BR/EDR SCO and eSCO
            Number of vendor codecs: 0
    ......
    < HCI Command: Read Local Suppor.. (0x04|0x000e) plen 7
            Codec: mSBC (0x05)
            Logical Transport Type: 0x00
            Direction: Input (Host to Controller) (0x00)
    > HCI Event: Command Complete (0x0e) plen 12
          Read Local Supported Codec Capabilities (0x04|0x000e) ncmd 1
            Status: Success (0x00)
            Number of codec capabilities: 1
             Capabilities #0:
            00 00 11 15 02 33
    
    Signed-off-by: Kiran K <kiran.k@intel.com>
    Signed-off-by: Chethan T N <chethan.tumkur.narayan@intel.com>
    Signed-off-by: Srivatsa Ravishankar <ravishankar.srivatsa@intel.com>

--- net/bluetooth/hci_core.c
+++ net/bluetooth/hci_core.c
@@ -1006,6 +1006,28 @@ void hci_codec_list_parse(struct hci_dev *hdev, __u8 num_codecs,
 	}
 }
 
+static void hci_codec_list_parse_v2(struct hci_dev *hdev, __u8 num_codecs,
+				    void *codec_list, bool is_vendor_codec)
+{
+	__u8 i;
+
+	for (i = 0; i < num_codecs; i++) {
+		if (!is_vendor_codec) {
+			struct hci_standard_codecs_v2 *codecs = codec_list;
+
+			hci_read_codec_capabilities(hdev, &codecs->codec[i],
+						    codecs->codec[i].transport,
+						    is_vendor_codec);
+		} else {
+			struct hci_vendor_codecs_v2 *codecs = codec_list;
+
+			hci_read_codec_capabilities(hdev, &codecs->codec[i],
+						    codecs->codec[i].transport,
+						    is_vendor_codec);
+		}
+	}
+}
+
 static void hci_read_supported_codecs(struct hci_dev *hdev)
 {
 	struct sk_buff *skb;
@@ -1056,10 +1078,62 @@ error:
 	kfree_skb(skb);
 }
 
+static void hci_read_supported_codecs_v2(struct hci_dev *hdev)
+{
+	struct sk_buff *skb;
+	struct hci_rp_read_local_supported_codecs_v2 *rp;
+	struct hci_standard_codecs_v2 *std_codecs;
+	struct hci_vendor_codecs_v2 *vendor_codecs;
+
+	skb = __hci_cmd_sync(hdev, HCI_OP_READ_LOCAL_CODECS_V2, 0, NULL,
+			     HCI_CMD_TIMEOUT);
+
+	if (IS_ERR(skb)) {
+		bt_dev_err(hdev, "Failed to read local supported codecs (%ld)",
+			   PTR_ERR(skb));
+		return;
+	}
+
+	if (skb->len < sizeof(*rp))
+		goto error;
+
+	rp = (void *)skb->data;
+
+	if (rp->status)
+		goto error;
+
+	skb_pull(skb, sizeof(rp->status));
+
+	std_codecs = (void *)skb->data;
+
+	if (skb->len < flex_array_size(std_codecs, codec, std_codecs->num)
+	    + sizeof(std_codecs->num))
+		goto error;
+
+	hci_codec_list_parse_v2(hdev, std_codecs->num, std_codecs, false);
+
+	skb_pull(skb, flex_array_size(std_codecs, codec, std_codecs->num)
+		 + sizeof(std_codecs->num));
+
+	vendor_codecs = (void *)skb->data;
+
+	if (skb->len <
+	    flex_array_size(vendor_codecs, codec, vendor_codecs->num)
+	    + sizeof(vendor_codecs->num))
+		goto error;
+
+	hci_codec_list_parse_v2(hdev, vendor_codecs->num, vendor_codecs, true);
+
+error:
+	kfree_skb(skb);
+}
+
 static void hci_init5_req(struct hci_dev *hdev)
 {
 	/* Read local codec list if the HCI command is supported */
-	if (hdev->commands[29] & 0x20)
+	if (hdev->commands[45] & 0x04)
+		hci_read_supported_codecs_v2(hdev);
+	else if (hdev->commands[29] & 0x20)
 		hci_read_supported_codecs(hdev);
 }
 
