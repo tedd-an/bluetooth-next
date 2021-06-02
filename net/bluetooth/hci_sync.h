04fffd4dafb1e668b7ae3bd5838140e97f63d479
Luiz Augusto von Dentz <luiz.von.dentz@intel.com>
2021-06-02 15:54:02 -0700

    Bluetooth: hci_sync: Make use of hci_cmd_sync_queue set 1
    This make use of hci_cmd_sync_queue for the following MGMT commands:
    
    Set Device Class
    Set Device ID
    Add UUID
    Remove UUID
    
    tools/mgmt-tester -s "Set Device Class"
    
    Test Summary
    ------------
    Set Device Class - Success 1                         Passed       0.024 seconds
    Set Device Class - Success 2                         Passed       0.019 seconds
    Set Device Class - Invalid parameters 1              Passed       0.014 seconds
    Total: 3, Passed: 3 (100.0%), Failed: 0, Not Run: 0
    Overall execution time: 0.0599 seconds
    
    tools/mgmt-tester -s "Set Device ID"
    
    Test Summary
    ------------
    Set Device ID - Success 1                            Passed       0.019 seconds
    Set Device ID - Success 2                            Passed       0.016 seconds
    Set Device ID - Disable                              Passed       0.014 seconds
    Set Device ID - Power off and Power on               Passed       0.019 seconds
    Set Device ID - SSP off and Power on                 Passed       0.017 seconds
    Set Device ID - Invalid Parameter                    Passed       0.015 seconds
    Total: 6, Passed: 6 (100.0%), Failed: 0, Not Run: 0
    Overall execution time: 0.107 seconds
    
    tools/mgmt-tester -s "Add UUID"
    
    Test Summary
    ------------
    Add UUID - UUID-16 1                                 Passed       0.023 seconds
    Add UUID - UUID-16 multiple 1                        Passed       0.018 seconds
    Add UUID - UUID-16 partial 1                         Passed       0.024 seconds
    Add UUID - UUID-32 1                                 Passed       0.015 seconds
    Add UUID - UUID-32 multiple 1                        Passed       0.017 seconds
    Add UUID - UUID-32 partial 1                         Passed       0.020 seconds
    Add UUID - UUID-128 1                                Passed       0.016 seconds
    Add UUID - UUID-128 multiple 1                       Passed       0.017 seconds
    Add UUID - UUID-128 partial 1                        Passed       0.018 seconds
    Add UUID - UUID mix                                  Passed       0.017 seconds
    Total: 10, Passed: 10 (100.0%), Failed: 0, Not Run: 0
    Overall execution time: 0.198 seconds
    
    tools/mgmt-tester -s "Remove UUID"
    
    Test Summary
    ------------
    Remove UUID - Success 1                              Passed       0.022 seconds
    Remove UUID - All UUID - Success 2                   Passed       0.017 seconds
    Remove UUID - Power Off - Success 3                  Passed       0.015 seconds
    Remove UUID - Power Off and On - Success 4           Passed       0.017 seconds
    Remove UUID - Not Exist - Invalid Params 1           Passed       0.015 seconds
    Total: 5, Passed: 5 (100.0%), Failed: 0, Not Run: 0
    Overall execution time: 0.0908 seconds
    
    Signed-off-by: Luiz Augusto von Dentz <luiz.von.dentz@intel.com>

---
+++ net/bluetooth/hci_sync.h
@@ -0,0 +1,9 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+/*
+ * BlueZ - Bluetooth protocol stack for Linux
+ *
+ * Copyright (C) 2021 Intel Corporation
+ */
+
+int hci_update_eir_sync(struct hci_dev *hdev);
+int hci_update_class_sync(struct hci_dev *hdev);
