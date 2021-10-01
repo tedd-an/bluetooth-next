32281962700b2a9cf9f77e609e3f96dfbca482a1
Luiz Augusto von Dentz <luiz.von.dentz@intel.com>
2021-09-30 17:47:16 -0700

    Bluetooth: Rename driver .prevent_wake to .wakeup
    prevent_wake logic is backward since what it is really checking is
    if the device may wakeup the system or not, not that it can prevent
    the system to be awaken.
    
    Also looking on how other subsystems have the entry as power/wakeup
    this also renames the force_prevent_wake to just wakeup in vhci driver.
    
    Signed-off-by: Luiz Augusto von Dentz <luiz.von.dentz@intel.com>

--- drivers/bluetooth/btusb.c
+++ drivers/bluetooth/btusb.c
@@ -3636,7 +3636,7 @@ static void btusb_check_needs_reset_resume(struct usb_interface *intf)
 		interface_to_usbdev(intf)->quirks |= USB_QUIRK_RESET_RESUME;
 }
 
-static bool btusb_prevent_wake(struct hci_dev *hdev)
+static bool btusb_wakeup(struct hci_dev *hdev)
 {
 	struct btusb_data *data = hci_get_drvdata(hdev);
 
@@ -3797,7 +3797,7 @@ static int btusb_probe(struct usb_interface *intf,
 	hdev->flush  = btusb_flush;
 	hdev->send   = btusb_send_frame;
 	hdev->notify = btusb_notify;
-	hdev->prevent_wake = btusb_prevent_wake;
+	hdev->wakeup = btusb_wakeup;
 
 #ifdef CONFIG_PM
 	err = btusb_config_oob_wake(hdev);
