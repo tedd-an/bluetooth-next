8992980beaf2ced9a058a8a10551220d25e7a1dd
Pauli Virtanen <pav@iki.fi>
2023-06-01 09:34:46 +0300

    Bluetooth: ISO: do not emit new LE Create CIS if previous is pending
    LE Create CIS command shall not be sent before all CIS Established
    events from its previous invocation have been processed. Currently it is
    sent via hci_sync but that only waits for the first event, but there can
    be multiple.
    
    Make it wait for all events, and simplify the CIS creation as follows:
    
    Add new flag HCI_CONN_CREATE_CIS, which is set if Create CIS has been
    sent for the connection but it is not yet completed.
    
    Make BT_CONNECT state to mean the connection wants Create CIS.
    
    On events after which new Create CIS may need to be sent, send it if
    possible and some connections need it. These events are:
    hci_connect_cis, iso_connect_cfm, hci_cs_le_create_cis,
    hci_le_cis_estabilished_evt.
    
    The Create CIS status/completion events shall queue new Create CIS only
    if at least one of the connections transitions away from BT_CONNECT, so
    that we don't loop if controller is sending bogus events.
    
    This fixes sending multiple CIS Create for the same CIS in the
    "ISO AC 6(i) - Success" BlueZ test case:
    
    < HCI Command: LE Create Co.. (0x08|0x0064) plen 9  #129 [hci0]
            Number of CIS: 2
            CIS Handle: 257
            ACL Handle: 42
            CIS Handle: 258
            ACL Handle: 42
    > HCI Event: Command Status (0x0f) plen 4           #130 [hci0]
          LE Create Connected Isochronous Stream (0x08|0x0064) ncmd 1
            Status: Success (0x00)
    > HCI Event: LE Meta Event (0x3e) plen 29           #131 [hci0]
          LE Connected Isochronous Stream Established (0x19)
            Status: Success (0x00)
            Connection Handle: 257
            ...
    < HCI Command: LE Setup Is.. (0x08|0x006e) plen 13  #132 [hci0]
            ...
    > HCI Event: Command Complete (0x0e) plen 6         #133 [hci0]
          LE Setup Isochronous Data Path (0x08|0x006e) ncmd 1
            ...
    < HCI Command: LE Create Co.. (0x08|0x0064) plen 5  #134 [hci0]
            Number of CIS: 1
            CIS Handle: 258
            ACL Handle: 42
    > HCI Event: Command Status (0x0f) plen 4           #135 [hci0]
          LE Create Connected Isochronous Stream (0x08|0x0064) ncmd 1
            Status: ACL Connection Already Exists (0x0b)
    > HCI Event: LE Meta Event (0x3e) plen 29           #136 [hci0]
          LE Connected Isochronous Stream Established (0x19)
            Status: Success (0x00)
            Connection Handle: 258
            ...
    
    Fixes: c09b80be6ffc ("Bluetooth: hci_conn: Fix not waiting for HCI_EVT_LE_CIS_ESTABLISHED")
    Signed-off-by: Pauli Virtanen <pav@iki.fi>

--- net/bluetooth/hci_event.c
+++ net/bluetooth/hci_event.c
@@ -3807,6 +3807,7 @@ static u8 hci_cc_le_set_cig_params(struct hci_dev *hdev, void *data,
 	struct hci_cp_le_set_cig_params *cp;
 	struct hci_conn *conn;
 	u8 status = rp->status;
+	bool pending = false;
 	int i;
 
 	bt_dev_dbg(hdev, "status 0x%2.2x", rp->status);
@@ -3855,13 +3856,8 @@ static u8 hci_cc_le_set_cig_params(struct hci_dev *hdev, void *data,
 			bt_dev_dbg(hdev, "%p handle 0x%4.4x parent %p",
 				   conn, conn->handle, conn->parent);
 
-			/* Create CIS if LE is already connected */
-			if (conn->parent &&
-			    conn->parent->state == BT_CONNECTED) {
-				rcu_read_unlock();
-				hci_le_create_cis(conn);
-				rcu_read_lock();
-			}
+			if (conn->state == BT_CONNECT)
+				pending = true;
 
 			break;
 		}
@@ -3870,6 +3866,9 @@ static u8 hci_cc_le_set_cig_params(struct hci_dev *hdev, void *data,
 	rcu_read_unlock();
 
 unlock:
+	if (pending)
+		hci_le_create_cis_pending(hdev);
+
 	hci_dev_unlock(hdev);
 
 	return rp->status;
@@ -4235,6 +4234,7 @@ static void hci_cmd_complete_evt(struct hci_dev *hdev, void *data,
 static void hci_cs_le_create_cis(struct hci_dev *hdev, u8 status)
 {
 	struct hci_cp_le_create_cis *cp;
+	bool pending = false;
 	int i;
 
 	bt_dev_dbg(hdev, "status 0x%2.2x", status);
@@ -4257,11 +4257,17 @@ static void hci_cs_le_create_cis(struct hci_dev *hdev, u8 status)
 
 		conn = hci_conn_hash_lookup_handle(hdev, handle);
 		if (conn) {
+			if (test_and_clear_bit(HCI_CONN_CREATE_CIS,
+					       &conn->flags))
+				pending = true;
 			conn->state = BT_CLOSED;
 			hci_connect_cfm(conn, status);
 			hci_conn_del(conn);
 		}
 	}
+
+	if (pending)
+		hci_le_create_cis_pending(hdev);
 
 	hci_dev_unlock(hdev);
 }
@@ -6805,6 +6811,7 @@ static void hci_le_cis_estabilished_evt(struct hci_dev *hdev, void *data,
 {
 	struct hci_evt_le_cis_established *ev = data;
 	struct hci_conn *conn;
+	bool pending = false;
 	u16 handle = __le16_to_cpu(ev->handle);
 
 	bt_dev_dbg(hdev, "status 0x%2.2x", ev->status);
@@ -6825,6 +6832,8 @@ static void hci_le_cis_estabilished_evt(struct hci_dev *hdev, void *data,
 			   handle);
 		goto unlock;
 	}
+
+	pending = test_and_clear_bit(HCI_CONN_CREATE_CIS, &conn->flags);
 
 	if (conn->role == HCI_ROLE_SLAVE) {
 		__le32 interval;
@@ -6851,10 +6860,14 @@ static void hci_le_cis_estabilished_evt(struct hci_dev *hdev, void *data,
 		goto unlock;
 	}
 
+	conn->state = BT_CLOSED;
 	hci_connect_cfm(conn, ev->status);
 	hci_conn_del(conn);
 
 unlock:
+	if (pending)
+		hci_le_create_cis_pending(hdev);
+
 	hci_dev_unlock(hdev);
 }
 
