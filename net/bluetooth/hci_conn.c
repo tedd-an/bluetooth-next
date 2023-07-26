eb044a39b5d3973f22699ada065006ef3ee200e7
Pauli Virtanen <pav@iki.fi>
2023-07-27 00:25:26 +0300

    Bluetooth: ISO: handle bound CIS cleanup via hci_conn
    Calling hci_conn_del in __iso_sock_close is invalid. It needs
    hdev->lock, but it cannot be acquired there due to lock ordering.
    
    Fix this by doing cleanup via hci_conn_drop.
    
    Return hci_conn with refcount 1 from hci_bind_cis and hci_connect_cis,
    so that the iso_conn always holds one reference.  This also fixes
    refcounting when error handling.
    
    Since hci_conn_abort shall handle termination of connections in any
    state properly, we can handle BT_CONNECT socket state in the same way as
    BT_CONNECTED.
    
    Signed-off-by: Pauli Virtanen <pav@iki.fi>

--- net/bluetooth/hci_conn.c
+++ net/bluetooth/hci_conn.c
@@ -1901,6 +1901,8 @@ struct hci_conn *hci_bind_cis(struct hci_dev *hdev, bdaddr_t *dst,
 		return ERR_PTR(-EINVAL);
 	}
 
+	hci_conn_hold(cis);
+
 	cis->iso_qos = *qos;
 	cis->state = BT_BOUND;
 
@@ -2253,6 +2255,9 @@ struct hci_conn *hci_connect_cis(struct hci_dev *hdev, bdaddr_t *dst,
 		hci_conn_drop(cis);
 		return ERR_PTR(-ENOLINK);
 	}
+
+	/* Link takes the refcount */
+	hci_conn_drop(cis);
 
 	cis->state = BT_CONNECT;
 
