12410cbbfb0afde7c53512c5d5e409f974b95b6b
Luiz Augusto von Dentz <luiz.von.dentz@intel.com>
2023-09-14 14:03:58 -0700

    Bluetooth: L2CAP: Fix leaking l2cap_conn objects
    l2cap_conn objects must be cleanup whenever its hci_chan is deleted by
    hci_chan_list_flush since it means the connection attempt is being
    aborted prematurely thus no calls to connect_cfm/disconnect_cfm would
    be generated causing the l2cap_conn object to leak.
    
    Fixes: 73d80deb7bdf ("Bluetooth: prioritizing data over HCI")
    Reported-by: Olivier L'Heureux <olivier.lheureux@fortrobotics.com>
    Signed-off-by: Luiz Augusto von Dentz <luiz.von.dentz@intel.com>

--- net/bluetooth/hci_conn.c
+++ net/bluetooth/hci_conn.c
@@ -2746,6 +2746,10 @@ void hci_chan_del(struct hci_chan *chan)
 	/* Prevent new hci_chan's to be created for this hci_conn */
 	set_bit(HCI_CONN_DROP, &conn->flags);
 
+	if (chan->cleanup)
+		chan->cleanup(chan);
+
+	chan->conn = NULL;
 	hci_conn_put(conn);
 
 	skb_queue_purge(&chan->data_q);
