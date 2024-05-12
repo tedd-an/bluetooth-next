382abe5acb5e605c1ef7a9d453af6d901a3c9fc5
Hector Martin <marcan@marcan.st>
2024-05-12 12:12:07 +0000

    Bluetooth: hci_bcm4377: Increase boot timeout
    BCM4388 takes over 2 seconds to boot, so increase the timeout (and also
    fix the units while we're here).
    
    Signed-off-by: Hector Martin <marcan@marcan.st>
    Reviewed-by: Sven Peter <sven@svenpeter.dev>
    Signed-off-by: Sven Peter <sven@svenpeter.dev>

--- drivers/bluetooth/hci_bcm4377.c
+++ drivers/bluetooth/hci_bcm4377.c
@@ -32,7 +32,8 @@ enum bcm4377_chip {
 #define BCM4378_DEVICE_ID 0x5f69
 #define BCM4387_DEVICE_ID 0x5f71
 
-#define BCM4377_TIMEOUT 1000
+#define BCM4377_TIMEOUT msecs_to_jiffies(1000)
+#define BCM4377_BOOT_TIMEOUT msecs_to_jiffies(5000)
 
 /*
  * These devices only support DMA transactions inside a 32bit window
@@ -1857,7 +1858,7 @@ static int bcm4377_boot(struct bcm4377_data *bcm4377)
 	dev_dbg(&bcm4377->pdev->dev, "waiting for firmware to boot\n");
 
 	ret = wait_for_completion_interruptible_timeout(&bcm4377->event,
-							BCM4377_TIMEOUT);
+							BCM4377_BOOT_TIMEOUT);
 	if (ret == 0) {
 		ret = -ETIMEDOUT;
 		goto out_dma_free;
