03d1f657c6416fef5d1ce4c07cf32c49a48a426f
Luiz Augusto von Dentz <luiz.von.dentz@intel.com>
2024-12-04 11:58:49 -0500

    Bluetooth: hci_core: Fix sleeping function called from invalid context
    This reworks hci_cb_list to not use mutex hci_cb_list_lock to avoid bugs
    like the bellow:
    
    BUG: sleeping function called from invalid context at kernel/locking/mutex.c:585
    in_atomic(): 0, irqs_disabled(): 0, non_block: 0, pid: 5070, name: kworker/u9:2
    preempt_count: 0, expected: 0
    RCU nest depth: 1, expected: 0
    4 locks held by kworker/u9:2/5070:
     #0: ffff888015be3948 ((wq_completion)hci0#2){+.+.}-{0:0}, at: process_one_work kernel/workqueue.c:3229 [inline]
     #0: ffff888015be3948 ((wq_completion)hci0#2){+.+.}-{0:0}, at: process_scheduled_works+0x8e0/0x1770 kernel/workqueue.c:3335
     #1: ffffc90003b6fd00 ((work_completion)(&hdev->rx_work)){+.+.}-{0:0}, at: process_one_work kernel/workqueue.c:3230 [inline]
     #1: ffffc90003b6fd00 ((work_completion)(&hdev->rx_work)){+.+.}-{0:0}, at: process_scheduled_works+0x91b/0x1770 kernel/workqueue.c:3335
     #2: ffff8880665d0078 (&hdev->lock){+.+.}-{3:3}, at: hci_le_create_big_complete_evt+0xcf/0xae0 net/bluetooth/hci_event.c:6914
     #3: ffffffff8e132020 (rcu_read_lock){....}-{1:2}, at: rcu_lock_acquire include/linux/rcupdate.h:298 [inline]
     #3: ffffffff8e132020 (rcu_read_lock){....}-{1:2}, at: rcu_read_lock include/linux/rcupdate.h:750 [inline]
     #3: ffffffff8e132020 (rcu_read_lock){....}-{1:2}, at: hci_le_create_big_complete_evt+0xdb/0xae0 net/bluetooth/hci_event.c:6915
    CPU: 0 PID: 5070 Comm: kworker/u9:2 Not tainted 6.8.0-syzkaller-08073-g480e035fc4c7 #0
    Hardware name: Google Google Compute Engine/Google Compute Engine, BIOS Google 03/27/2024
    Workqueue: hci0 hci_rx_work
    Call Trace:
     <TASK>
     __dump_stack lib/dump_stack.c:88 [inline]
     dump_stack_lvl+0x241/0x360 lib/dump_stack.c:114
     __might_resched+0x5d4/0x780 kernel/sched/core.c:10187
     __mutex_lock_common kernel/locking/mutex.c:585 [inline]
     __mutex_lock+0xc1/0xd70 kernel/locking/mutex.c:752
     hci_connect_cfm include/net/bluetooth/hci_core.h:2004 [inline]
     hci_le_create_big_complete_evt+0x3d9/0xae0 net/bluetooth/hci_event.c:6939
     hci_event_func net/bluetooth/hci_event.c:7514 [inline]
     hci_event_packet+0xa53/0x1540 net/bluetooth/hci_event.c:7569
     hci_rx_work+0x3e8/0xca0 net/bluetooth/hci_core.c:4171
     process_one_work kernel/workqueue.c:3254 [inline]
     process_scheduled_works+0xa00/0x1770 kernel/workqueue.c:3335
     worker_thread+0x86d/0xd70 kernel/workqueue.c:3416
     kthread+0x2f0/0x390 kernel/kthread.c:388
     ret_from_fork+0x4b/0x80 arch/x86/kernel/process.c:147
     ret_from_fork_asm+0x1a/0x30 arch/x86/entry/entry_64.S:243
     </TASK>
    
    Reported-by: syzbot+2fb0835e0c9cefc34614@syzkaller.appspotmail.com
    Closes: https://syzkaller.appspot.com/bug?extid=2fb0835e0c9cefc34614
    Signed-off-by: Luiz Augusto von Dentz <luiz.von.dentz@intel.com>

--- net/bluetooth/iso.c
+++ net/bluetooth/iso.c
@@ -2154,6 +2154,11 @@ done:
 	return HCI_LM_ACCEPT;
 }
 
+static bool iso_match(struct hci_conn *hcon)
+{
+	return hcon->type == ISO_LINK || hcon->type == LE_LINK;
+}
+
 static void iso_connect_cfm(struct hci_conn *hcon, __u8 status)
 {
 	if (hcon->type != ISO_LINK) {
@@ -2335,6 +2340,7 @@ drop:
 
 static struct hci_cb iso_cb = {
 	.name		= "ISO",
+	.match		= iso_match,
 	.connect_cfm	= iso_connect_cfm,
 	.disconn_cfm	= iso_disconn_cfm,
 };
