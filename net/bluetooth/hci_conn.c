f68a3d214339045d7ea222598ffa8cfb3b50ccb7
Pauli Virtanen <pav@iki.fi>
2025-09-21 22:14:25 +0300

    Bluetooth: hci_conn: extend conn_hash lookup RCU critical sections
    When doing hci_conn_hash lookups with only RCU, extend critical section
    to cover also the handling of the return value.
    
    This avoids any concurrent hci_conn_del from deleting the conn before we
    are done dereferencing it.
    
    Fixes: a091289218202 ("Bluetooth: hci_conn: Fix hci_le_set_cig_params")
    Signed-off-by: Pauli Virtanen <pav@iki.fi>

--- net/bluetooth/hci_conn.c
+++ net/bluetooth/hci_conn.c
@@ -1813,9 +1813,13 @@ static int set_cig_params_sync(struct hci_dev *hdev, void *data)
 	u8 aux_num_cis = 0;
 	u8 cis_id;
 
+	rcu_read_lock();
+
 	conn = hci_conn_hash_lookup_cig(hdev, cig_id);
-	if (!conn)
+	if (!conn) {
+		rcu_read_unlock();
 		return 0;
+	}
 
 	qos = &conn->iso_qos;
 	pdu->cig_id = cig_id;
@@ -1853,6 +1857,8 @@ static int set_cig_params_sync(struct hci_dev *hdev, void *data)
 		cis->p_rtn  = qos->ucast.in.rtn;
 	}
 	pdu->num_cis = aux_num_cis;
+
+	rcu_read_unlock();
 
 	if (!pdu->num_cis)
 		return 0;
