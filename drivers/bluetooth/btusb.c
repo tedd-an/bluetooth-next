0b09355c63c17f10866ced9869bd703bc207a7b3
Techie Ernie <techieernie@gmail.com>
2025-12-24 00:28:38 +0800

    Bluetooth: btusb: Add Realtek 8851BE USB ID (0489:e112)
    Add USB ID 0489:e112 for the Realtek 8851BE Bluetooth adapter.
    Without this entry, the device is not handled correctly by btusb.
    Adding the ID enables proper Realtek initialization for Bluetooth to work on various motherboards using this Bluetooth adapter.
    
    The device identifies as:
      Bus 001 Device XXX: ID 0489:e112 Foxconn / Hon Hai Bluetooth Radio
    
    Tested on Realtek 8851BE.
    
    Signed-off-by: Techie Ernie <techieernie@gmail.com>

--- drivers/bluetooth/btusb.c
+++ drivers/bluetooth/btusb.c
@@ -522,6 +522,12 @@ static const struct usb_device_id quirks_table[] = {
 	{ USB_DEVICE(0x13d3, 0x3600), .driver_info = BTUSB_REALTEK },
 	{ USB_DEVICE(0x13d3, 0x3601), .driver_info = BTUSB_REALTEK },
 
+
+	/* Additional Realtek 8851BE Bluetooth devices */
+	{ USB_DEVICE(0x0489, 0xe112), .driver_info = BTUSB_REALTEK |
+						     BTUSB_WIDEBAND_SPEECH },
+
+
 	/* Realtek 8851BU Bluetooth devices */
 	{ USB_DEVICE(0x3625, 0x010b), .driver_info = BTUSB_REALTEK |
 						     BTUSB_WIDEBAND_SPEECH },
