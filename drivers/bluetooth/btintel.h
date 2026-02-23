04680d9d074164f948b4f11813314f61cfb60773
Kiran K <kiran.k@intel.com>
2026-02-23 22:51:00 +0530

    Bluetooth: btintel: Add DSBR support for ScP2 onwards
    Add DSBR support for Scorpious Peak2 cores onwards.
    
    Refer commit eb9e749c0182 ("Bluetooth: btintel: Allow configuring drive
    strength of BRI") for details about DSBR.
    
    Signed-off-by: Kiran K <kiran.k@intel.com>

--- drivers/bluetooth/btintel.h
+++ drivers/bluetooth/btintel.h
@@ -74,6 +74,7 @@ struct intel_tlv {
 #define BTINTEL_HWID_BZRI	0x1e
 #define BTINTEL_HWID_BZRU	0x1d
 #define BTINTEL_HWID_SCP	0x1f
+#define BTINTEL_HWID_SCP2	0x20
 #define BTINTEL_HWID_BZRIW	0x22
 
 struct intel_version_tlv {
